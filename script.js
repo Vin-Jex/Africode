39cea8355d9d910a5ed37cd7478522377d1e3a9f
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-10-10 12:47:02 +0100

    Did basic DOM manipulation

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const menu = document.querySelector("#menu");
-const times = document.querySelector(".times");
-const nav = document.querySelector(".header-main")
+const toggle = document.querySelector(".toggle");
+const nav = document.querySelector(".header-main");
+const link = document.querySelectorAll(".link")
+const otherTarget = document.querySelectorAll(".link.active")
 
-menu.addEventListener('click', (e) => {
-  if (nav.style.display = "none") {
-    nav.style.display = "flex"
-    menu.style.display = "none"
-    times.style.display = "block"
-  } else if (nav.style.display = "flex") {
-    nav.style.display = "none"
-    menu.style.display = "none"
-  }
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle("show-nav");
+  nav.classList.toggle("show-nav")
+});
 
-  
-  
+link.forEach((links) => {
+  links.addEventListener("click", (link) => {
+    if (link.classList = "active") {
+      link.classList.remove('active')
+      link.target.classList.add('active')
+      // console.log(e.target.classList);
+    }
+  })
 })
+
+
